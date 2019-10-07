ee2860b18bc2f02bddfcf73911986108510da9b7
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2019-10-07 16:15:37 +0200

    static

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -167,11 +167,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = env('STATIC_URL', default='/django-static/')
 
 
 CORS_ORIGIN_WHITELIST = (
